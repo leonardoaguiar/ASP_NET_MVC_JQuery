26b29e34b7d8c3a52ec0b707af06bce2c417f34e
leonardoaguiar <leonardo.r.aguiar@gmail.com>
2012-06-14 17:31:21 -0300

    Escrita do Código
    Código obtido no link http://goo.gl/Zouzq e usado para estudo de AJAX
    com JQuery no ASP.NET MVC.

---
+++ TutorialMVCJquery/TutorialMVCJquery/Controllers/CadastroController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TutorialMVCJquery.Controllers
+{
+    public class CadastroController : Controller
+    {
+        //
+        // GET: /Cadastro/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Cadastrar(FormCollection formulario)
+        {
+            string retorno = null;
+            retorno = "Nome: " + formulario["nome"] + " Email: " + formulario["email"];
+            return retorno;
+        }
+
+    }
+}
